1775f9ebefa794ae15512ae82073a1c13810d0ee
piousankrah99 <133825879+piousankrah99@users.noreply.github.com>
2024-03-10 08:12:45 +0000

    Update Snipers.java

--- psniper-main/psniper-main/primesniper/src/main/java/primesniper/Snipers.java
+++ psniper-main/psniper-main/primesniper/src/main/java/primesniper/Snipers.java
@@ -8,7 +8,9 @@ import javax.persistence.*;
 @Entity
 @Table(name = "primesniperitemsdb")
 @AllArgsConstructor
-
+@Getter
+@Setter
+@ToString
 public class Snipers {
 
     public Snipers() {
@@ -22,42 +24,5 @@ public class Snipers {
     private String item;
     private float price;
 
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-
-
-    public String getItem() {
-        return item;
-    }
-
-
-
-
-    public void setItem(String item) {
-        this.item = item;
-    }
-
-
-
-    public float getPrice() {
-        return price;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "Sniper{" +
-                "id=" + id +
-                ", item='" + item + '\'' +
-                ", price=" + price +
-                '}';
-    }
+   
 }
